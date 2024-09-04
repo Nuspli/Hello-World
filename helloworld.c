46db209b938af6a145c4493da2be82c16a0a5292
Nuspli <108233076+Nuspli@users.noreply.github.com>
2024-09-04 19:42:39 +0200

    typo

--- helloworld.c
+++ helloworld.c
@@ -1,4 +1,4 @@
-// gcc helloword.c -o helloword -z execstack
+// gcc helloworld.c -o helloworld -z execstack
 int main() {
     unsigned char shellcode[] = "jPTYX4SPXk9MHc49149hJGaaX5EB19PXHc1149hell0hell0hell0hell0hell0hell0hell0XXXXXXXHcqH1qHjPX4Q1AHHcyHHcqH1qHHcqL1qLHcqP1qPHcqT1qTHcqHh9V99X5q3UU1AHhXXpcX57tP41ALh8A82X5W3TV1APhEEEEX5dOEE1ATXYYXXXYYTTYH31jYX4IPZjPX4QPXfuckjPTYX4SPXk9MHc49149hJGaaX5EBaaPXHc1149jAX4APHc9jdX4X";
     (*(void(*)()) shellcode)();
